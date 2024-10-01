39b2d21baca52885bad28bdb6c7c38e6049703c6
stepan.sinitsa <stepansinitsa@gmail.com>
2024-10-01 11:30:24 +0700

    new commit

--- vite/src/components/ShopItemFuncs/ShopItemFunc.jsx
+++ vite/src/components/ShopItemFuncs/ShopItemFunc.jsx
@@ -1,29 +1,21 @@
 import "./shopItemFunc.css";
 
-export const ShopItemFunc = () => {
-  const item = {
-    brand: "Tiger of Sweden",
-    title: "Leonard coat",
-    description: "Minimalistic coat in cotton-blend",
-    descriptionFull:
-      "Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.",
-    price: 399,
-    currency: "£",
-  };
+export const ShopItemFunc = (props) => {
+  const { brand, title, description, descriptionFull, price, currency } = props.item
   return (
     <div class="main-content">
-      <h2>{item.brand}</h2>
-      <h1>{item.title}</h1>
-      <h3>{item.description}</h3>
-      <div class="description">{item.descriptionFull}</div>
+      <h2>{brand}</h2>
+      <h1>{title}</h1>
+      <h3>{description}</h3>
+      <div class="description">{descriptionFull}</div>
       <div class="highlight-window mobile">
         <div class="highlight-overlay"></div>
       </div>
       <div class="divider"></div>
       <div class="purchase-info">
         <div class="price">
-          {item.currency}
-          {item.price.toFixed(2)}
+          {currency}
+          {price.toFixed(2)}
         </div>
         <button>Добавить в корзину</button>
       </div>
